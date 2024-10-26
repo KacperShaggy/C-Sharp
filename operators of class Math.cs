b5901380608fcfa2047076b37956db5cf43140d5
Kacper <113052358+KacperShaggy@users.noreply.github.com>
2024-10-27 01:28:34 +0200

    Add files via upload

---
+++ operators of class Math.cs
@@ -0,0 +1,21 @@
+﻿string x = Console.ReadLine();
+double y = Convert.ToDouble(x);
+
+//PODNOSZENIE DO POTĘGI
+double a = Math.Pow(y, 2); 
+Console.WriteLine($"{y} do kwadratu: {a}\n");
+
+//POTĘGA
+double b = Math.Sqrt(y);
+Console.WriteLine($"{y} pod potęgą wynosi: {b}\n");
+
+//WARTOŚĆ BEZWZGLĘDNA
+double c = Math.Abs(y);
+Console.WriteLine($"{y} w wartości bezględnej: {c}\n");
+
+//ZAOKRĄGLANIE
+double d = Math.Round(y);
+Console.WriteLine($"{y} zaokrąglone: {d}\n");
+
+
+
