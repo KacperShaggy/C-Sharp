e63eb9cdbadb86d55ea559b2f4c8f44fd90d7f4f
Kacper <113052358+KacperShaggy@users.noreply.github.com>
2024-10-27 01:51:44 +0200

    Add files via upload

---
+++ number guessing game.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Zgadnij liczbe z zakresu (0,100>");
+
+int attempt = 0;
+bool right = false;
+
+Random random = new Random();
+int x = random.Next(1, 100);
+
+while (!right)
+{
+    string str = Console.ReadLine();
+    int inp = Convert.ToInt32(str);
+
+    if (inp > x)
+    {
+        Console.WriteLine("that number is lower!");
+
+        attempt++;
+    }
+    else if (inp < x)
+    {
+        Console.WriteLine("that number is higher!");
+        attempt++;
+    }
+    else if (inp == x)
+    {
+        Console.WriteLine($"youre right! thats it! number: {x} \n numbers of attempts: {attempt}");
+        right = true;
+    }
+    else
+    {
+        Console.WriteLine("input error, try again");
+    }
+}
